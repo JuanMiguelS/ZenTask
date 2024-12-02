6b3a757ccb796839ebf51cb73755523591c3a905
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2024-12-02 10:12:22 +0100

    counter-page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,18 @@ import { HeaderComponent } from '../app/components/header/header.component';
 import { ItemComponent } from "./components/item/item.component";
 import { TotalComponent } from "./components/total/total.component";
 import { TranslateModule } from '@ngx-translate/core'; // Asegúrate de que TranslateModule esté importado
+import { CounterPageComponent } from './components/counter-page/counter-page.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    FormsModule,
     RouterOutlet,
     ItemsComponent,
     HeaderComponent,
+    CounterPageComponent,
     ItemComponent,
     TotalComponent,
     TranslateModule // Asegúrate de que TranslateModule está en los imports del componente
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { ItemsComponent } from './components/items/items.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { Component } from '@angular/core';
 import { ConfigComponent } from './components/config/config.component';
+import { CounterPageComponent } from './components/counter-page/counter-page.component';
 export const routes: Routes = [
   { path: '', component: ItemsComponent },  // Página principal
   { path: 'add', component: AddItemComponent },  // Página de agregar item
   {path: 'app-config', component: ConfigComponent}, //Pagina de Ajustes y configuración
+  {path: 'app-counter-page', component: CounterPageComponent},
 
 ];
